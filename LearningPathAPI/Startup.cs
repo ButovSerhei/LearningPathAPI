7d20d331389a5a8b5cd4183f9f6593005f9896a7
SerheiButov <s.butov@qcode.pro>
2020-08-02 15:32:17 +0300

    Some impovements

--- LearningPathAPI/Startup.cs
+++ LearningPathAPI/Startup.cs
@@ -1,34 +1,93 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
+using System.IO.Compression;
 using System.Linq;
-using System.Threading.Tasks;
 using LearningPath.SmtpManager;
+using LearningPathApi.WebApplication.MiddleWare;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace LearningPathAPI
+namespace LearningPathApi.WebApplication
 {
     public class Startup
     {
-
+        public IWebHostEnvironment Environment { get; }
         public IConfiguration Configuration { get; }
-        public Startup(IConfiguration configuration)
+
+
+        public Startup(IWebHostEnvironment env, IConfiguration configuration)
         {
-            Configuration = configuration;
+            var cfgbuilder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = cfgbuilder.Build();
+            Environment = env;
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRazorPages().AddRazorRuntimeCompilation();
+
+            services.AddSession(options =>
+            {
+                options.Cookie.Name = ".LearningPath.Session";
+                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddResponseCompression(options =>
+            {
+                options.EnableForHttps = true;
+                options.Providers.Add<BrotliCompressionProvider>();
+                options.Providers.Add<GzipCompressionProvider>();
+                options.MimeTypes =
+                    ResponseCompressionDefaults.MimeTypes.Concat(
+                        new[]
+                        {
+                            "font/woff2",
+                            "application/font-woff2",
+                            "application/font-woff",
+                            "application/font",
+                            "application/javascript",
+                            "application/x-javascript",
+                            "application/javascript; charset=utf-8"
+                        });
+            });
+            services.Configure<BrotliCompressionProviderOptions>(options =>
+            {
+                options.Level = CompressionLevel.Fastest;
+            });
+            services.Configure<GzipCompressionProviderOptions>(options =>
+            {
+                options.Level = CompressionLevel.Fastest;
+            });
+
+            services.AddResponseCaching();
+
+            services.AddControllers();
+
             services.AddRouting(options => options.LowercaseUrls = true);
             services.AddMvc(option => option.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
                 .AddNewtonsoftJson(options => options.SerializerSettings.Culture = CultureInfo.GetCultureInfo("en-US"));
+
             services.AddSingleton(typeof(ISmtpManager), typeof(SmtpManager));
         }
 
@@ -40,16 +99,23 @@ namespace LearningPathAPI
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseRewriter(new RewriteOptions().AddRedirectToHttps(StatusCodes.Status301MovedPermanently, 443));
+            app.UseMiddleware<DeChunkerMiddleware>();
+            app.UseHttpsRedirection();
+            app.UseResponseCompression();
+            app.UseStaticFiles();
+
             app.UseSession();
             app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller = Home}/{action = Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
-            
         }
     }
 }
